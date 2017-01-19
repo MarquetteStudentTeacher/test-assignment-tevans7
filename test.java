df5b753a24128d416e3e43389d11c91d47d94c2a
Travis Menghini <menghinitravis@gmail.com>
2017-01-18 22:24:45 -0600

    Revert "Revert "I just added another line""
    This reverts commit a8bab0347ff6e11317236a6870e4b4bd8e9af5e9.
    
    # Conflicts:
    #	test.java

--- test.java
+++ test.java
@@ -3,6 +3,10 @@ public class test
 	public static void main(string[] args)
 	{
 		//Type here
+<<<<<<< HEAD
 		//Change from a branch
+=======
+		//Another change is here
+>>>>>>> parent of a8bab03... Revert "I just added another line"
 	}
 }
